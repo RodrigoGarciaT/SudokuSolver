6aede93df02f5316644c1009a0a42d3ab39d204c
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2022-09-01 12:53:26 -0500

    Built the Sudoku grid and changed the color of the numbers to black

--- sudokugame.py
+++ sudokugame.py
@@ -1,7 +1,8 @@
 import pygame
 
-def redrawGameWindow(win):
-    win.fill((0, 0, 0))
+def redrawGameWindow(win,width,rows):
+    win.fill((255, 255, 255))
+    draw_board(width,rows,win)
     for number in numberList:
         number.draw_number(win)
     pygame.display.update()
@@ -14,7 +15,7 @@ class numbers():
 
     def draw_number(self,win):
         font = pygame.font.SysFont('comicsans', 30, True)
-        text = font.render(str(self.number), 1, (255, 0, 255))
+        text = font.render(str(self.number), 1, (0, 0, 0))
         win.blit(text, self.pos)
 
     def position_number(self):
@@ -23,15 +24,32 @@ class numbers():
 numberList =[]
 
 
-def draw_board():
+def draw_board(win):
     pass
+
+
+def draw_board(w, rows, surface):
+    sizeBtwn = w // rows  # Gives us the distance between the lines
+
+    x = 0  # Keeps track of the current x
+    y = 0  # Keeps track of the current y
+    for l in range(rows):  # We will draw one vertical and one horizontal line each loop
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (0,0,0), (x,0),(x,w))
+        pygame.draw.line(surface, (0,0,0), (0,y),(w,y))
 
 
 def main():
     pygame.init()
+    height = 500
+    width = 500
+    rows = 9
     win = pygame.display.set_mode((500, 500))
     pygame.display.set_caption("First Game")
     run = True
+    pos = (0, 0)
     while run:
 
         for event in pygame.event.get():
@@ -39,7 +57,6 @@ def main():
                 run = False
 
         left, middle, right = pygame.mouse.get_pressed()
-
         if left:
             mouse_pressed = True
             print("Left Mouse Key is being pressed")
@@ -53,8 +70,7 @@ def main():
             if keys[num_key[i]]:
                 number = i+1
                 numberList.append(numbers(number, pos))
-        redrawGameWindow(win)
-
+        redrawGameWindow(win,width,rows)
     pygame.quit()
 
 
