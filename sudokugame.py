ea4aa62e62d5f1bea07cb3f2fffc1af35e81cadf
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2022-09-03 12:20:49 -0500

    Made it so that the user can now plug in numbers into the sudoku.

--- sudokugame.py
+++ sudokugame.py
@@ -4,7 +4,7 @@ def redrawGameWindow(win,width,rows):
     win.fill((255, 255, 255))
     draw_board(width,rows,win)
     for number in numberList:
-        number.draw_number(win)
+        number.draw_number(win,width,rows)
     pygame.display.update()
 
 
@@ -13,40 +13,39 @@ class numbers():
         self.pos = pos
         self.number = number
 
-    def draw_number(self,win):
+    def draw_number(self,win,w,rows):
         font = pygame.font.SysFont('comicsans', 30, True)
         text = font.render(str(self.number), 1, (0, 0, 0))
+        self.position_number(w,rows)
         win.blit(text, self.pos)
 
-    def position_number(self):
-        pass
+    def position_number(self,w,rows):
+        self.pos=(self.pos[0] // (w // rows)*55+(55//2)-10, self.pos[1]//(w // rows)*55+(55//5))
+
+
 
 numberList =[]
-
-
-def draw_board(win):
-    pass
 
 
 def draw_board(w, rows, surface):
     sizeBtwn = w // rows  # Gives us the distance between the lines
 
-    x = 0  # Keeps track of the current x
-    y = 0  # Keeps track of the current y
-    for l in range(rows):  # We will draw one vertical and one horizontal line each loop
+    x = 50  # Keeps track of the current x
+    y = 50  # Keeps track of the current y
+    startPoint=50
+    for l in range(rows+1):  # We will draw one vertical and one horizontal line each loop
+        bold = 3 if not l%3 else 1
+        pygame.draw.line(surface, (0,0,0), (x,50),(x,w+startPoint),bold)
+        pygame.draw.line(surface, (0,0,0), (50,y),(w+startPoint,y),bold)
         x = x + sizeBtwn
         y = y + sizeBtwn
-
-        pygame.draw.line(surface, (0,0,0), (x,0),(x,w))
-        pygame.draw.line(surface, (0,0,0), (0,y),(w,y))
-
 
 def main():
     pygame.init()
     height = 500
     width = 500
     rows = 9
-    win = pygame.display.set_mode((500, 500))
+    win = pygame.display.set_mode((1000, 1000))
     pygame.display.set_caption("First Game")
     run = True
     pos = (0, 0)
@@ -70,7 +69,8 @@ def main():
             if keys[num_key[i]]:
                 number = i+1
                 numberList.append(numbers(number, pos))
-        redrawGameWindow(win,width,rows)
+                print((pos[0] // (width // rows))*55, ' ', (pos[1]//(width // rows))*55)
+        redrawGameWindow(win, width, rows)
     pygame.quit()
 
 
